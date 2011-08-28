c3e4e70b892d7e929464026fa73d9f69be5e4bbd
Nejc Zupan <nejc.zupan@gmail.com>
2011-08-28 13:26:32 +0200

    Preparing release 0.1.1

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.2'
+version = '0.1.1'
 
 setup(name='niteoweb.fabfile',
       version=version,
       description="A bunch of Fabric commands we use all the time.",
       long_description=read('README') +
-                       read('docs', 'HISTORY.rst') +
-                       read('docs', 'LICENSE.rst'),
+                       read('docs', 'HISTORY.txt') +
+                       read('docs', 'LICENSE.txt'),
       classifiers=[
         "Programming Language :: Python",
         ],
