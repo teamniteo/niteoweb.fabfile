1afc8602c2718caac5615f86e1f011a48b78e52e
Nejc Zupan <nejc.zupan@gmail.com>
2011-09-04 11:59:11 +0200

    we can just import * from project.py, no need to explicitly specify which methods

--- niteoweb/fabfile/__init__.py
+++ niteoweb/fabfile/__init__.py
@@ -1,16 +1,4 @@
 # for legacy reasongs -> code was moved from __init__.py
 # to project.py -> we import it here so these methods are
 # still accessible for example by ``from niteoweb.fabfile configure_nginx``
-from niteoweb.fabfile.project import configure_nginx
-from niteoweb.fabfile.project import upload_configuration
-from niteoweb.fabfile.project import enable_configuration
-from niteoweb.fabfile.project import download_code
-from niteoweb.fabfile.project import prepare_buildout
-from niteoweb.fabfile.project import run_buildout
-from niteoweb.fabfile.project import upload_data
-from niteoweb.fabfile.project import upload_zodb
-from niteoweb.fabfile.project import upload_blobs
-from niteoweb.fabfile.project import start_supervisord
-from niteoweb.fabfile.project import supervisorctl
-from niteoweb.fabfile.project import download_data
-from niteoweb.fabfile.project import upload_sphinx
+from niteoweb.fabfile.project import *
