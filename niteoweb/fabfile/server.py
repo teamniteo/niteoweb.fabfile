78beaae292ea97dc72fa7adcf792a8c2ba2c9f15
Nejc Zupan <nejc.zupan@gmail.com>
2011-10-07 13:56:34 +0200

    add create_maintenance_user()

--- niteoweb/fabfile/server.py
+++ niteoweb/fabfile/server.py
@@ -1,6 +1,9 @@
+from fabric.api import env
 from fabric.api import sudo
+from fabric.contrib.files import append
 from fabric.contrib.files import sed
 from fabric.contrib.files import uncomment
+from fabric.operations import prompt
 
 
 def install_rkhunter():
@@ -17,3 +20,25 @@ def install_rkhunter():
     uncomment('/etc/rkhunter.conf', '#ALLOWHIDDENDIR=\/dev\/.udev', use_sudo=True)
     uncomment('/etc/rkhunter.conf', '#ALLOWHIDDENDIR=\/dev\/.static', use_sudo=True)
     uncomment('/etc/rkhunter.conf', '#ALLOWHIDDENDIR=\/dev\/.initramfs', use_sudo=True)
+
+
+def create_maintenance_user(admin):
+    """Create an account for an admin to use to access the server."""
+    env.admin = admin
+
+    # create user
+    sudo('egrep %(admin)s /etc/passwd || adduser %(admin)s --disabled-password --gecos ""' % env)
+
+    # add public key for SSH access
+    sudo('mkdir /home/%(admin)s/.ssh' % env)
+    env.pub = prompt("Paste admin's public key: ")
+    sudo("echo '%(pub)s' > /home/%(admin)s/.ssh/authorized_keys" % env)
+
+    # allow this user in sshd_config
+    append('AllowUsers %(admin)s@*', use_sudo=True)
+
+    # allow sudo for maintenance user by adding it to 'sudo' group
+    sudo('gpasswd -a %(admin)s sudo' % env)
+
+    # set default password for initial login
+    sudo('echo "%(admin)s:geslo123" | chpasswd' % env)
