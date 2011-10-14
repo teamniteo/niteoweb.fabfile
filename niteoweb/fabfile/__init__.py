4508fb087595bc157f97235b3551690d76e47b0e
Andraz Brodnik <brodul@gmx.com>
2011-10-14 13:46:04 +0200

    new function for error raising

--- niteoweb/fabfile/__init__.py
+++ niteoweb/fabfile/__init__.py
@@ -2,3 +2,7 @@
 # to project.py -> we import it here so these methods are
 # still accessible for example by ``from niteoweb.fabfile configure_nginx``
 from niteoweb.fabfile.project import *
+
+
+def err(msg):
+    raise(msg)
