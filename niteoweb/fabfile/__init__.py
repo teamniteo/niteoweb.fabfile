c8518b86d2f203b0ee58067f03978bfe98eea86b
Nejc Zupan <nejc.zupan@gmail.com>
2011-10-24 00:58:57 +0200

    backwards compatibility breached: no longer import * from projects.py

--- niteoweb/fabfile/__init__.py
+++ niteoweb/fabfile/__init__.py
@@ -1,8 +1,2 @@
-# for legacy reasongs -> code was moved from __init__.py
-# to project.py -> we import it here so these methods are
-# still accessible for example by ``from niteoweb.fabfile configure_nginx``
-from niteoweb.fabfile.project import *
-
-
 def err(msg):
     raise AttributeError(msg)
