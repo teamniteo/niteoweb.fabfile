860d8bd4b2bc67f461f6f5e0daf14ae67dfdf634
Nejc Zupan <nejc.zupan@gmail.com>
2011-11-15 15:22:36 +0100

    Preparing release 2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.0.2'
+version = '2.1'
 
 setup(name='niteoweb.fabfile',
       version=version,
